8af40c90e73c4f8b33941e0ca9d5b0f0f75972df
lioneljones <lioneljones5116@gmail.com>
2022-08-17 11:57:47 -0500

    More Code

---
+++ RealmDBGroceryApp/Models/ShoppingListItem.swift
@@ -0,0 +1,22 @@
+//
+//  ShoppingListItem.swift
+//  RealmDBGroceryApp
+//
+//  Created by lionel jones on 8/17/22.
+//
+
+import Foundation
+import RealmSwift
+
+class ShoppingListItem: Object, Identifiable {
+    
+    //Persisted means that this property will be allowed to persist in Realm Storage
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var title: String
+    @Persisted var quantity: Int
+    
+    override class func primaryKey() -> String? {
+        "id"
+    }
+    
+}
