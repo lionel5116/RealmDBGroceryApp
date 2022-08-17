ae8e0320aeab251ac600e19195f4d42485640232
lioneljones <lioneljones5116@gmail.com>
2022-08-17 07:50:27 -0500

    Initial Commit

---
+++ RealmDBGroceryApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealmDBGroceryApp
+//
+//  Created by lionel jones on 8/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RealmDBGroceryApp/RealmDBGroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmDBGroceryAppApp.swift
+//  RealmDBGroceryApp
+//
+//  Created by lionel jones on 8/17/22.
+//
+
+import SwiftUI
+
+@main
+struct RealmDBGroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
